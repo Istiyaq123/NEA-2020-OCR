e2e40511159830993fd3991750de393bab32fd3e
Istiyaq123 <ahmedistiyaqqq03@gmail.com>
2020-01-29 18:24:56 +0000

    don

---
+++ NEA.py
@@ -0,0 +1,37 @@
+#Function for loging up
+def logup():
+    users = open('users.txt','a')
+    usernameOne = input('What is your name ')
+    usernameOneP = input('What is your password. Note it must be larger than 3 letters ')
+    Ua = ('\n'+usernameOne + '\t' + usernameOneP)
+    users.write(Ua)
+    while len(usernameOneP) < 3:
+        usernameOneP = input('Error ')
+        Ua = ('\n'+usernameOne + '\t' + usernameOneP)
+        users.write(Ua)
+    users.close()
+
+def login():
+    users = open('users.txt','r')
+    usernameone = input('What is your name')
+    usernameonepassword = input('What is your password')
+    Ua = (usernameone + '\t' + usernameonepassword)
+    for line in users:
+        if line == Ua:
+            print('Why')
+        else:
+            print('okay')
+
+#Ask The User if they Have an Account
+log = input('Do You Have an Account ').title()
+if log == 'Yes':
+    login()
+elif log == 'Y':
+    login()
+elif log == 'No':
+    logup()
+elif log == 'N':
+    logup()
+else:
+    print('Program Has Crashed, Please Try Again')
+    quit()
