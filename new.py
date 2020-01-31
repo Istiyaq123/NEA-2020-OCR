717c3abe74e0b95d8a61e4df54fc85b5a189ad58
Istiyaq123 <ahmedistiyaqqq03@gmail.com>
2020-01-31 17:48:36 +0000

    hi

--- new.py
+++ new.py
@@ -1,3 +1,11 @@
+#initialiser
+p1_score = 0
+p2_score = 0
+roll = 0
+
+#import random
+import random
+
 #define login
 def login():
     usernameone = input('Enter Username One Username: ')
@@ -20,6 +28,27 @@ def logup():
     while len(passwordtwo)<3:
         passwordtwo = input('Error ')
 
+#define play()
+def play():
+    number = random.randint(1,6)
+    p1_roll = input('Player One Roll. Yes or No').title()
+    if p1_roll == 'Yes':
+        print(number)
+        if number <6:
+            p1_score = p1_score + number
+            check = number %2
+            if check %2:
+                p1_score = p1_score + 10
+            else:
+                p1_score = p1_score - 5
+        elif number == 6:
+            p1_score = p1_score + number
+            number = random.randint(1,6)
+            print(number)
+            p1_score = p1_score + number
+
+
+#Loging in or loging up
 log = input('Do you have an account? ').title()
 if log == 'Yes':
     login()
@@ -29,3 +58,14 @@ elif log == 'No':
     logup()
 elif log == 'N':
     logup()
+else:
+    print('Program has crashed.\nPlease try again.')
+    quit()
+
+#Rules
+print('\nEvery player has 5 rolls')
+print('If you roll and even number ten points is added to your score\nIf you roll and odd number 5 points is subtracted from your score')
+
+#Play
+
+play()
